2e84cf94becfa4c06d0f9b9f0a3aad81e9a1548a
Никита Лесняк <nikitalesnak@MacBook-Pro-Nikita.local>
2022-05-16 02:25:31 +0300

    feat: images

--- components/featured/index.tsx
+++ components/featured/index.tsx
@@ -1,30 +1,23 @@
 import React from "react";
 
-import useTranslation from "@hooks/translation";
 import { PaintingI } from "@api/types";
 
 import ItemList from "@components/works_list";
-import lng from "./language.json";
 import styles from "./style.module.scss";
 
 interface FeaturedI {
   paintings: PaintingI[];
 }
 
-const Featured: React.FC<FeaturedI> = ({ paintings }) => {
-  const n = useTranslation(lng as any);
+const Featured: React.FC<FeaturedI> = ({ paintings }) => (
+  <section className={styles.main}>
+    <div className="container">
+      <div className={styles.wrapper}>
+        {/* <h2 className={styles.title}>{n("featured.title")}</h2> */}
 
-  return (
-    <section className={styles.main}>
-      <div className="container">
-        <div className={styles.wrapper}>
-          {/* <h2 className={styles.title}>{n("featured.title")}</h2> */}
-
-          <ItemList paintings={paintings} />
-        </div>
+        <ItemList paintings={paintings} />
       </div>
-    </section>
-  );
-};
-
+    </div>
+  </section>
+);
 export default Featured;
