0bcc22d1c8d43b7d8ce4e5007c402be44d606b4a
Никита Лесняк <nikitalesnak@MacBook-Pro-Nikita.local>
2022-06-05 20:56:55 +0300

    feat: clouds

--- components/hero/three/index.tsx
+++ components/hero/three/index.tsx
@@ -10,12 +10,10 @@ const Three = () => {
       <mesh scale={100}>
         <boxGeometry args={[1, 1, 1]} />
         <LayerMaterial side={THREE.BackSide}>
-          {/* <Color color="black" alpha={1} /> */}
-
           <Depth
-            colorA="#171514"
-            colorB="#515151"
-            // colorA="#8D3E18"
+            // colorA="#171514"
+            colorA="#f66b2a"
+            colorB="skyblue"
             alpha={1}
             mode="normal"
             near={130}
@@ -33,12 +31,6 @@ const Three = () => {
             alpha={0.2}
           />
         </LayerMaterial>
-        {/* <Sky
-          distance={450000}
-          sunPosition={[0, 1, 0]}
-          inclination={0}
-          azimuth={180}
-        /> */}
       </mesh>
     );
   }
@@ -54,32 +46,19 @@ const Three = () => {
 
   return (
     <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 10], fov: 22 }}>
-      {/* <mesh>
-        <boxBufferGeometry attach="geometry" />
-        <meshStandardMaterial />
-      </mesh> */}
-
       <fog attach="fog" args={["black", 0, 130]} />
 
-      <directionalLight color="white" position={[0, 0, 5]} />
+      <directionalLight color="black" position={[0, 0, 5]} />
 
       <Suspense fallback={null}>
         <Rig />
 
-        {/* <Cloud
+        <Cloud
           opacity={0.5}
           speed={0.4} // Rotation speed
           width={10} // Width of the full cloud
           depth={1.5} // Z-dir depth
-          segments={20} // Number of particles
-        /> */}
-
-        <Cloud
-          opacity={0.8}
-          speed={0.4} // Rotation speed
-          width={10} // Width of the full cloud
-          depth={1.5} // Z-dir depth
-          segments={18} // Number of particles
+          segments={10} // Number of particles
         />
       </Suspense>
 
